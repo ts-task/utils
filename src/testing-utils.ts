56344e7fd1b127232d592fca1c497c3aaf429de2
Gonza Gluzman <dggluz@gmail.com>
2018-08-14 15:50:46 -0300

    feat(case-error): added caseError function

---
+++ src/testing-utils.ts
@@ -0,0 +1,16 @@
+import { UncaughtError } from '@acamica/task'
+
+export const jestAssertNever = (cb: jest.DoneCallback) => (obj: never | UncaughtError) =>
+  cb('this should never happen', obj)
+
+export const jestAssertUntypedNeverCalled = (cb: jest.DoneCallback) => (obj: any) =>
+  cb('this should never happen', obj)
+
+export const assertFork = <T>(cb: jest.DoneCallback, fn: (obj: T) => void) => (obj: T) => {
+  try {
+    fn(obj)
+    cb()
+  } catch (err) {
+    cb(err)
+  }
+}
