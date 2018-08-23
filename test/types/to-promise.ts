be27a1a83f18304a9751a3fb78aee405fc76576f
Gonza Gluzman <dggluz@gmail.com>
2018-08-23 18:03:32 -0300

    chore(test/types): fixed formatting (added semicolons)

--- test/types/to-promise.ts
+++ test/types/to-promise.ts
@@ -1,23 +1,24 @@
-import { toPromise } from '@ts-task/utils'
-import { Task } from '@acamica/task'
+import { toPromise } from '@ts-task/utils';
+import { Task } from '@acamica/task';
 
 // We will test `toPromise` that just transform a Task into a Promise, losing the typings on the error.
 
 // NoNegativesError is the error we are interested in.
 // It has a `NoNegativesError` public property because TypeScript inference is structural
 class NoNegativesError extends Error {
-  NoNegativesError = 'NoNegativesError'
+  NoNegativesError = 'NoNegativesError';
 }
 
 // rejectNegative is a function that will return a Task possible rejected with a NoNegativesError
 const rejectNegative = (x: number): Task<number, NoNegativesError> =>
   x >= 0 ? Task.resolve(x) : Task.reject(new NoNegativesError())
+;
 
 // GIVEN: a Task, typed on the success and on the error...
-const tNumber = rejectNegative(9) // $ExpectType Task<number, NoNegativesError>
+const tNumber = rejectNegative(9); // $ExpectType Task<number, NoNegativesError>
 
 // WHEN: calling `toPromise`...
-const pNumber = toPromise(tNumber)
+const pNumber = toPromise(tNumber);
 
 // THEN: the result is a Promise typed on the Task's success type (and losing its error type)
-pNumber // $ExpectType Promise<number>
+pNumber; // $ExpectType Promise<number>
